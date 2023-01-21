71654eb5cd4933226edc725d566b342bd7243269
visheshgargavi <63608636+visheshgargavi@users.noreply.github.com>
2023-01-21 16:20:17 +0530

    Update __init__.py

--- blobTrigger-outblob/BlobTrigger3/__init__.py
+++ blobTrigger-outblob/BlobTrigger3/__init__.py
@@ -4,8 +4,10 @@ import azure.functions as func
 
 
 def main(myblob: func.InputStream,outputBlob: func.Out[str]) -> None:
+    clear_text = myblob.read().decode('utf-8')
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
-    outputBlob.set(myblob.to_string())
+                 f"Blob Size: {myblob.length} bytes\n"
+                 f"{clear_text}")
+    outputBlob.set(clear_text)
     logging.info("Success!")   
