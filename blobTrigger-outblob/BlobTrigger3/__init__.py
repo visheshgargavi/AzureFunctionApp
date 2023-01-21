192db1b44587c491d3458c47611244ae44d9d0db
visheshgargavi <63608636+visheshgargavi@users.noreply.github.com>
2023-01-21 14:31:11 +0530

    Update __init__.py

--- blobTrigger-outblob/BlobTrigger3/__init__.py
+++ blobTrigger-outblob/BlobTrigger3/__init__.py
@@ -3,7 +3,7 @@ import logging
 import azure.functions as func
 
 
-def main(myblob: func.InputStream,outputBlob: func.Out[bytes]) -> None:
+def main(myblob: func.InputStream,outputBlob: func.Out[str]) -> None:
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
